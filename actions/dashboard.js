4e10ccb15d90df2030d99bb225dc88e9354593af
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-12 05:39:35 +0530

    Created Account Drawer and Custom Hook to Fetch APIs

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -2,6 +2,7 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 const serializeTransaction = (obj) => {
   const serialized = { ...obj };
