db8f593b3b2df82f41ad1c2ec3c181d5ce535351
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-15 03:51:09 +0530

    Edit Transaction

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -2,16 +2,30 @@ import React from "react";
 import { getUserAccounts } from "@/actions/dashboard";
 import { defaultCategories } from "@/data/categories";
 import AddTransactionForm from "../_components/transaction-form";
+import { getTransaction } from "@/actions/transaction";
 
-const AddTransactionPage = async () => {
+const AddTransactionPage = async ({ searchParams }) => {
   const accounts = await getUserAccounts();
+
+  const editId = searchParams?.edit;
+
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
 
   return (
     <div className="max-w-3xl mx-auto px-5">
       <h1 className="text-5xl bg-gradient-to-br from-blue-600 to-purple-600 bg-clip-text text-transparent font-extrabold tracking-tight mb-8">
-        Add Transaction
+        {editId?"Edit":"Add"} Transaction
       </h1>
-      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
     </div>
   );
 };
