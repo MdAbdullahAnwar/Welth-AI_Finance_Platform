4ef69c889fe072e08deae013781cc83a9d747824
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-15 06:43:30 +0530

    Dashboard Expense Pie Chart

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,10 +1,12 @@
-import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
 import { getCurrentBudget } from "@/actions/budget";
 import AccountCard from "./_components/account-card";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import BudgetProgress from "./_components/budget-progress";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
+import { Suspense } from "react";
+import { DashboardOverview } from "./_components/transaction-overview";
 
 export default async function DashboardPage() {
   const accounts = await getUserAccounts();
@@ -15,8 +17,11 @@ export default async function DashboardPage() {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
 
+  const transactions = await getDashboardData();
+
   return (
     <div className="space-y-8">
+      {/* Budget Progress */}
       {defaultAccount && (
         <BudgetProgress
           initialBudget={budgetData?.budget}
@@ -24,6 +29,15 @@ export default async function DashboardPage() {
         />
       )}
 
+      {/* Overview */}
+      <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview
+          accounts={accounts}
+          transactions={transactions || []}
+        />
+      </Suspense>
+
+      {/* Account Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
           <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
--- components/hero.jsx
+++ components/hero.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com">
+          <Link href="https://github.com/MdAbdullahAnwar/Welth-AI_Finance_Platform.git">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
